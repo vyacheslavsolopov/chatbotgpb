658ad8ae4917976346321ca9317cb7d01f3abb00
Aleksandr Boldyrev <alseboldyrev@edu.hse.ru>
2025-04-30 17:04:42 +0300

    - fixed json

--- chat/views.py
+++ chat/views.py
@@ -41,7 +41,7 @@ def api_chat(request):
         text_list = llm_client.call(prompt_buttons).get('llm_response', '')
 
         try:
-            text_list = text_list.strip('```').strip('json').replace("'", '"')
+            text_list = text_list.strip('```').strip('json').replace("'", '"').strip('```').strip('json')
             print(text_list)
             buttons = [{'text': t['question'].strip().capitalize()} for t in json.loads(text_list)]
         except Exception as e:
