a0b721f8b00b48243b8a6432da92ee7950271ff4
Aleksandr Boldyrev <alseboldyrev@edu.hse.ru>
2025-05-02 14:39:31 +0300

    - fixed buttons

--- chat/views.py
+++ chat/views.py
@@ -57,7 +57,7 @@ async def stream_llm_response(user_msg):
             cleaned_json = buttons_response.strip().lstrip('```json').rstrip('```').strip()
             cleaned_json = cleaned_json.strip().lstrip('```json').rstrip('```').strip()
             cleaned_json = cleaned_json.replace("'", '"')
-            cleaned_json = cleaned_json.replace('```', '').replace('json', '')
+            cleaned_json = cleaned_json.replace('```', '').replace('json', '').strip()
             print(cleaned_json)
             parsed_buttons = json.loads(cleaned_json)
             buttons = [{'text': item['question'].strip()} for item in parsed_buttons]
